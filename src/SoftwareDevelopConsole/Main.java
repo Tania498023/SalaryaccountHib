ebadd68968ae87e40c2b2487335085997f5326f4
Tania <perekhval@rambler.ru>
2022-12-26 23:22:32 +0300

    добавлен класс MemoryRepository

--- src/SoftwareDevelopConsole/Main.java
+++ src/SoftwareDevelopConsole/Main.java
@@ -4,17 +4,20 @@ import Persistence.FileRepository;
 import Persistence.MemoryRepository;
 import SoftwareDevelopDomain.Person.User;
 
+import java.io.IOException;
+import java.util.Collections;
+
 public class Main {
     public  static FileRepository fill;
     public static User polzovatel;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         int userRole = 0;
 
         fill = new FileRepository();//создаем экземпляры для возможности вызова метода FillFileUser
 
         var userReturn = new MemoryRepository();//создаем экземпляры для возможности вызова метода Users
-        fill.fillFileUser(userReturn.Users(), false);
+        fill.fillFileUser(Collections.singletonList(userReturn.Users()), false);
 
 
         var genericreturn = new MemoryRepository();
